e545b464897becca8e465539451bdba7a9e5d1bc
Yahor_Prys <yahor.prys@ukg.com>
2024-08-22 17:49:31 +0200

    task 5 text file

--- task 5.py
+++ task 5.py
@@ -1,68 +1,69 @@
+import sys
+from datetime import date, datetime
+
+
 class Publishing:
-    file = open('news_feed', 'a')
-    from datetime import date, datetime
+    def __init__(self):
+        self.type = 'Publication'
+        self.publication_content = None
+
+    def add_record(self):
+        with open('news_feed', 'a') as f:
+            f.write(self.publication_content)
 
 
 class AddNews(Publishing):
     def __init__(self, text, city):
+        super().__init__()
+        self.type = 'News'
+        self.header = self.type.ljust(40, '-')
         self.text = text
         self.city = city
-
-    def add_record(self, text, city):
-        type = 'News'
-        self.file.write(f"{type.ljust(40, '-')}\n{text}\n{city}, {AddNews.date.today()}\n\n")
-        self.file.close()
+        self.publication_content = f"{self.header}\n{self.text}\n{self.city}, {date.today()}\n\n"
 
 
 class AddAdvertising(Publishing):
     def __init__(self, text, date):
+        super().__init__()
+        self.type = 'Private Ad'
+        self.header = self.type.ljust(40, '-')
         self.text = text
-        self.date = date
-
-    def add_adv(self, text, date):
-        end_date = self.datetime.strptime(date, '%Y-%m-%d')
-        days_to_end = abs(self.datetime.today() - end_date)
-        type = 'Private Ad'
-        self.file.write(f"{type.ljust(40, '-')}\n{text}\nActual until: {end_date}, {days_to_end.days} days left\n\n")
-        self.file.close()
+        self.end_date = datetime.strptime(date, '%Y-%m-%d')
+        self.days_to_end = abs(datetime.today() - self.end_date)
+        self.publication_content = f"{self.header}\n{self.text}\nActual until: {self.end_date}, {self.days_to_end.days} days left\n\n"
 
 
 class AddAnnouncement(Publishing):
     def __init__(self, text, phone):
+        super().__init__()
+        self.type = 'Announcement'
+        self.header = self.type.ljust(40, '-')
         self.text = text
         self.phone = phone
-
-    def add_announcement(self, text, phone):
-        type = 'Announcement'
-        self.file.write(f"{type.ljust(40, '-')}\n{text}\nPhone: {phone}\n\n")
-        self.file.close()
+        self.publication_content = f"{self.header}\n{self.text}\nPhone: {self.phone}\n\n"
 
 
-class CheckType(AddNews, AddAdvertising, AddAnnouncement):
-    def __init__(self, text, city, date, phone, t=1):
-        AddNews.__init__(self, text, city)
-        AddAdvertising.__init__(self, text, date)
-        AddAnnouncement.__init__(self, text, phone)
-        self.t = t
-        self.text = text
-        self.city = city
-        self.date = date
-        self.phone = phone
-
-    def check_type(self, t):
-        if t == 1:
-            self.text = input('What happened?\n')
-            self.city = input('Where it happened?\n')
-            AddNews.add_record(self, self.text, self.city)
-        elif t == 2:
-            self.text = input('What do you promote?\n')
-            self.date = input('When is the last day of the ad?\n')
-            AddAdvertising.add_adv(self, self.text, self.date)
-        elif t == 3:
-            self.text = input('What do you announce?\n')
-            self.phone = input('What is your phone number?\n')
-            AddAnnouncement.add_announcement(self, self.text, self.phone)
+def check_type(t):
+    if t == 1:
+        text = input('What happened?\n')
+        city = input('Where it happened?\n')
+        news = AddNews(text, city)
+        news.add_record()
+    elif t == 2:
+        text = input('What do you promote?\n')
+        date = input('When is the last day of the ad?\n')
+        add = AddAdvertising(text, date)
+        add.add_record()
+    elif t == 3:
+        text = input('What do you announce?\n')
+        phone = input('What is your phone number?\n')
+        anounce = AddAnnouncement(text, phone)
+        anounce.add_record()
 
 
-a = CheckType('text', 'city', 'date', 'phone', 1)
-a.check_type(int(input('Select type of record. 1 - News, 2 - Ad, 3 - Announcement:\n')))
+while True:
+    marker = int(input('Select type of record. 1 - News, 2 - Ad, 3 - Announcement, 4 - For Exit:\n'))
+    if marker != 4:
+        check_type(marker)
+    else:
+        sys.exit()
