2dbf1d691e2a25f2e70195f97fd5a641cf541823
Yahor_Prys <yahor.prys@ukg.com>
2024-08-22 21:37:06 +0200

    task 6

--- task 5.py
+++ task 5.py
@@ -1,15 +1,32 @@
 import sys
+import os
 from datetime import date, datetime
+from task_4_1 import normalise_letter_case
 
 
 class Publishing:
     def __init__(self):
         self.type = 'Publication'
         self.publication_content = None
+        self.file = 'news_feed_data'
+        self.records_num = 1
 
     def add_record(self):
         with open('news_feed', 'a') as f:
             f.write(self.publication_content)
+
+    def read_records(self):
+        with open(self.file) as f2:
+            records = [x.split('\n') for x in normalise_letter_case(f2.read()).split('\n\n')]
+            lst = []
+            for i in records[:self.records_num]:
+                dicts = {}
+                dicts.setdefault('type', i[0])
+                dicts.setdefault('text', i[1])
+                dicts.setdefault('attribute', i[2])
+                lst.append(dicts)
+        os.remove(self.file)
+        return lst
 
 
 class AddNews(Publishing):
@@ -43,6 +60,13 @@ class AddAnnouncement(Publishing):
         self.publication_content = f"{self.header}\n{self.text}\nPhone: {self.phone}\n\n"
 
 
+class ReadTxt(Publishing):
+    def __init__(self, file, records_num):
+        super().__init__()
+        self.file = file
+        self.records_num = records_num
+
+
 def check_type(t):
     if t == 1:
         text = input('What happened?\n')
@@ -59,11 +83,26 @@ def check_type(t):
         phone = input('What is your phone number?\n')
         anounce = AddAnnouncement(text, phone)
         anounce.add_record()
+    elif t == 4:
+        file = input('Enter file name\n')
+        records_num = int(input('Enter the number of records\n'))
+        readfile = ReadTxt(file, records_num)
+        a = readfile.read_records()
+        for x in a:
+            if x['type'] == 'News':
+                news = AddNews(x['text'], x['attribute'])
+                news.add_record()
+            elif x['type'] == 'Private ad':
+                add = AddAdvertising(x['text'], x['attribute'])
+                add.add_record()
+            elif x['type'] == 'Announcement':
+                anounce = AddAnnouncement(x['text'], x['attribute'])
+                anounce.add_record()
 
 
 while True:
-    marker = int(input('Select type of record. 1 - News, 2 - Ad, 3 - Announcement, 4 - For Exit:\n'))
-    if marker != 4:
+    marker = int(input('Select type of record. 1 - News, 2 - Ad, 3 - Announcement, 4 - Upload from file, 5 - For Exit:\n'))
+    if marker != 5:
         check_type(marker)
     else:
         sys.exit()
